b17ef6e9d8754a5c20850641719b073364d17687
Alexey.Kharlanov@gmail.com <Alexey.Kharlanov@gmail.com@6bc91d6b-28a9-cc5f-efa9-c9988b34bfd3>
2012-02-02 06:57:04 +0000

    Reverted Motor mappings due error in original AC

--- ArduCopter/config_channels.h
+++ ArduCopter/config_channels.h
@@ -23,8 +23,6 @@
 // Output CH mapping for ArduCopter motor channels
 //
 //
-#define CH_INVALID (-1)
-
 #if CONFIG_APM_HARDWARE == APM_HARDWARE_APM2
 # define MOT_1 CH_1
 # define MOT_2 CH_2
@@ -35,57 +33,6 @@
 # define MOT_7 CH_7
 # define MOT_8 CH_8
 #else
-#if FRAME_CONFIG ==	QUAD_FRAME
-// X and Plus
-# define MOT_1 CH_1
-# define MOT_2 CH_2
-# define MOT_3 CH_3
-# define MOT_4 CH_4
-// Placeholders:
-# define MOT_5 CH_INVALID
-# define MOT_6 CH_INVALID
-# define MOT_7 CH_INVALID
-# define MOT_8 CH_INVALID
-#elif FRAME_CONFIG == TRI_FRAME
-# define MOT_1 CH_1
-# define MOT_2 CH_2
-# define MOT_3 CH_4
-// Placeholders:
-# define MOT_4 CH_INVALID
-# define MOT_5 CH_INVALID
-# define MOT_6 CH_INVALID
-# define MOT_7 CH_INVALID
-# define MOT_8 CH_INVALID
-#elif FRAME_CONFIG == HEXA_FRAME
-# define MOT_1 CH_7
-# define MOT_2 CH_3
-# define MOT_3 CH_2
-# define MOT_4 CH_8
-# define MOT_5 CH_4
-# define MOT_6 CH_1
-// Placeholders:
-# define MOT_7 CH_INVALID
-# define MOT_8 CH_INVALID
-#elif FRAME_CONFIG == Y6_FRAME
-# define MOT_1 CH_1
-# define MOT_2 CH_7
-# define MOT_3 CH_3
-# define MOT_4 CH_2
-# define MOT_5 CH_8
-# define MOT_6 CH_4
-// Placeholders:
-# define MOT_7 CH_INVALID
-# define MOT_8 CH_INVALID
-#elif FRAME_CONFIG == OCTA_FRAME
-# define MOT_1 CH_2
-# define MOT_2 CH_1
-# define MOT_3 CH_11
-# define MOT_4 CH_10
-# define MOT_5 CH_8
-# define MOT_6 CH_7
-# define MOT_7 CH_4
-# define MOT_8 CH_3
-#elif FRAME_CONFIG == OCTA_QUAD_FRAME
 # define MOT_1 CH_1
 # define MOT_2 CH_2
 # define MOT_3 CH_3
@@ -94,16 +41,6 @@
 # define MOT_6 CH_8
 # define MOT_7 CH_10
 # define MOT_8 CH_11
-#else // HELI
-# define MOT_1 CH_1
-# define MOT_2 CH_2
-# define MOT_3 CH_3
-# define MOT_4 CH_4
-# define MOT_5 CH_5
-# define MOT_6 CH_6
-# define MOT_7 CH_7
-# define MOT_8 CH_8
-#endif // FRAME_CONFIG
 #endif
 
 //
