206fbc52e0a983c5917a40cae11be906c40b405a
Alexey.Kharlanov@gmail.com <Alexey.Kharlanov@gmail.com@6bc91d6b-28a9-cc5f-efa9-c9988b34bfd3>
2012-04-04 18:39:49 +0000

    Remove unused libs

--- libraries/AP_GPS/AP_GPS.h
+++ libraries/AP_GPS/AP_GPS.h
@@ -5,14 +5,8 @@
 
 #include "AP_GPS_NMEA.h"
 #include "AP_GPS_SIRF.h"
-#include "AP_GPS_406.h"
 #include "AP_GPS_UBLOX.h"
-#include "AP_GPS_MTK.h"
 #include "AP_GPS_MTK16.h"
 #include "AP_GPS_None.h"
 #include "AP_GPS_Auto.h"
-#include "AP_GPS_HIL.h"
-#include "AP_GPS_Shim.h"	// obsoletes AP_GPS_HIL, use in preference
-#include "AP_GPS_IMU.h"		// temporarily reinstated for Xplane support
-//#include "AP_GPS_Ublox_i2c.h"
 #include "AP_GPS_BlackVortex.h"
